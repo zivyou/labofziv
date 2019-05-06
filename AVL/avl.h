4136062a1ca416b411f4bba60b052a29a4707b8b
ziv <ziv@office.mac>
2019-05-06 14:41:41 +0800

    update

--- AVL/avl.h
+++ AVL/avl.h
@@ -19,11 +19,136 @@ public:
     };
 };
 
-#define avlTree avlNode
+template<typename T>
+using avlTree = avlNode<T>;
+
+/*
+        tree           tree             N2
+          \              \             /  \
+          N1   =>        N2     =>   tree N1
+          /               \
+        N2                N1
+*/
+template<typename T>
+void rightLeftRotation(avlTree<T> *tree){
+    avlTree<T> *N1 = tree->rightChild;
+    avlTree<T> *N2 = N1->leftChild;
+    tree->rightChild = N2;
+    N1->leftChild = N2->rightChild;
+    N2->rightChild = N1;
+    return rightRightRotation(tree);
+}
+
+
+/*
+        tree                N1
+          \                / \
+          N1     =>     tree N2
+           \
+           N2
+*/
+template<typename T>
+void rightRightRotation(avlTree<T> *tree){
+    avlTree<T> *N1 = tree->rightChild;
+    avlTree<T> *N2 = N1->rightChild;
+    tree->rightChild = N1->leftChild;
+    N1->leftChild = tree;
+    return N1;
+}
+
+
+/*
+        tree            tree              N2
+        /                /               /  \
+       N1     =>       N2     =>        N1  tree
+        \              /
+        N2            N1
+*/
+template<typename T>
+void leftRightRotation(avlTree<T> *tree){
+    avlTree<T> *N1 = tree->leftChild;
+    avlTree<T> *N2 = N1->rightChild;
+    tree->leftChild = N2;
+    N1->rightChild = N2->leftChild;
+    N2->leftChild = N1;
+    return leftLeftRotation(tree);
+}
+
+
+/*
+        tree                  N1    
+        /                    /  \
+       N1      =>           N2  tree
+      /
+    N2
+*/
+template<typename T>
+void leftLeftRotation(avlTree<T> *tree){
+    avlTree<T> *N1 = tree->leftChild;
+    tree->leftChild = N1->rightChild;
+    N1->rightChild = tree;
+    return N1;
+}
+
+template<typename T>
+int getBalanceFactor(avlTree<T> *tree){
+    return getHeight(tree->leftChild) - getHeight(tree->rightChild);
+}
+
+
+template<typename T>
+int getHeight(avlTree<T> *tree){
+    if (!tree)
+        return 0;
+    return max<int>(getHeight(tree->leftChild), getHeight(tree->rightChild))+1;
+}
+
+template<typename T>
+bool avlBalanced(avlTree<T> *tree){
+    int lHeight = getHeight(tree->leftChild);
+    int rHeight = getHeight(tree->rightChild);
+    if (lHeight - rHeight <= 1 && rHeight - lHeight <= 1)
+        return true;
+    else
+        return false;
+}
+
+/**
+*   exact balanced
+*/
+template<typename T>
+bool balanced(avlTree<T> *tree){
+    if (getHeight(tree->leftChild) == getHeight(tree->rightChild))
+        return true;
+    else
+        return false;
+}
 
 template<typename T>
 avlTree<T> *balance(avlTree<T> *tree){
-
+    avlTree<T> *re;
+    int factor = getBalanceFactor(tree);
+    if (factor > 1){
+        if (getBalanceFactor(tree->leftChild) > 0){
+            // LL
+            re = leftLeftRotation(tree);
+        }else{
+            // LR
+            re = leftRightRotation(tree);
+        }
+    }else if (factor < -1) {
+        if (getBalanceFactor(tree->leftChild) > 0){
+            // RL
+            re = rightLeftRotation(tree);
+        }else{
+            // RR
+            re = rightRightRotation(tree);
+        }
+    }else
+        return tree;
+    if (!avlBalanced(re))
+        return balance(re);
+    return re;
 };
 
 template<typename T>
