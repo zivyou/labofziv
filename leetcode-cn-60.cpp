0cd04b001796d98100ef22d84adbf53969af4957
ziv <ziv@office.mac>
2019-05-23 10:19:34 +0800

    update

--- leetcode-cn-60.cpp
+++ leetcode-cn-60.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+    /*
     int factorial[10];
     string getPermutation(int n, int k) {
         string re;
@@ -30,14 +32,45 @@ public:
         int remain = k - begin*();
         re = re + std::to_string();
     }
+    */
+
+    void dfs(vector<vector<int> > &re, vector<int> &nums, int end, vector<int> &tmpRe, int count){
+        if (re.size() > count)
+            return;
+        if (end < 0){
+            re.push_back(tmpRe);
+            return;
+        }
+
+        int elem = nums[end];
+        for (int i=0; i<=tmpRe.size(); i++){
+            vector<int> newTmpRe = tmpRe;
+            newTmpRe.insert(newTmpRe.begin()+i, nums[end]);
+            dfs(re, nums, end-1, newTmpRe, count);
+        }
+    }
+
+    string getPermutation(int n, int k) {
+        vector<vector<int> > re;
+        vector<int> nums;
+        vector<int> tmpRe;
+        for (int i=1; i<=n; i++)
+            nums.push_back(i);
+        
+        dfs(re, nums, nums.size()-1, tmpRe, k);
+
+        for (int i=0; i<re.size(); i++){
+            for (int j=0; j<re[i].size(); j++)
+                cout<<re[i][j]<<" ";
+            cout<<endl;
+        }
+        
+        return "";
+    }
 };
 
 int main(){
-    int n = 9;
-    int re = 1;
-    for (int i=1; i<=n; i++){
-        re = re*i;
-        cout<<re<<endl;
-    }
+    Solution s;
+    s.getPermutation(3, 4);
     return 0;
 }
